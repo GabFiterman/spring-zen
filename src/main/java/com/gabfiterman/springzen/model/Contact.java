c15bd0327000346dc6536b72f2860673f501bcf2
GabFiterman <gfiterman96@gmail.com>
2023-11-13 11:04:13 -0300

    chore: add JPA and Lombok annotations

--- src/main/java/com/gabfiterman/springzen/model/Contact.java
+++ src/main/java/com/gabfiterman/springzen/model/Contact.java
@@ -1,7 +1,34 @@
 package com.gabfiterman.springzen.model;
 
-public record Contact(
-        String name,
-        String contact,
-        Professional professional) {
+import java.sql.Date;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "contact")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = {"id"})
+public class Contact {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
+        @Column(nullable = false)
+        private String name;
+
+        @Column(nullable = false)
+        private String contact;
+
+        @Column(name = "created_date", nullable = false)
+        private Date createdDate;
+
+        @ManyToOne
+        @JoinColumn(name = "professional_id", nullable = false)
+        private Professional professional;
 }
--- src/main/java/com/gabfiterman/springzen/model/Professional.java
+++ src/main/java/com/gabfiterman/springzen/model/Professional.java
@@ -2,9 +2,34 @@ package com.gabfiterman.springzen.model;
 
 import java.sql.Date;
 
-public record Professional(
-        String name,
-        ProfessionalRole role,
-        Date birthDate,
-        Date createdDate) {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "professional")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = {"id"})
+public class Professional {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
+        @Column(nullable = false)
+        private String name;
+
+        @Enumerated(EnumType.STRING)
+        @Column(nullable = false)
+        private ProfessionalRole role;
+
+        @Column(name = "birth_date", nullable = false)
+        private Date birthDate;
+
+        @Column(name = "created_date", nullable = false)
+        private Date createdDate;
 }
