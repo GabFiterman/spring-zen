18f6de31b92b6aea64d60a77d78b6f1243d9553e
GabFiterman <gfiterman96@gmail.com>
2023-11-12 22:32:17 -0300

    initial commit: created Spring-Application with some dependencies

---
+++ src/main/java/com/gabfiterman/springzen/SpringZenApplication.java
@@ -0,0 +1,13 @@
+package com.gabfiterman.springzen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringZenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringZenApplication.class, args);
+	}
+
+}
